c0cc2d06e2a5738b6b648a05e4a52ea62a3440fb
testddoser <129606911+testddoser@users.noreply.github.com>
2023-11-19 20:43:27 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1700415687,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/elena_zagruzka-sheet0.png",
+		"images/fon-sheet0.png",
+		"images/knopka-sheet0.png",
+		"images/vverx-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/photo_20231117_-sheet0.png",
+		"images/koma-sheet0.png",
+		"images/baesik-sheet0.png",
+		"images/djo-sheet0.png",
+		"images/topor-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/barsikgde-sheet0.png",
+		"images/babidze-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/picsart_-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
