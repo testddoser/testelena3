dfc3086c9d3b8a13598447e96ba0d9e8d82d6b22
testddoser <129606911+testddoser@users.noreply.github.com>
2023-11-20 15:40:16 +0300

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1700425143,
+	"version": 1700483951,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -24,6 +24,8 @@
 		"images/picsart_2-sheet0.png",
 		"images/photo_20231120_-sheet0.png",
 		"images/sprite10-sheet0.png",
+		"images/picsart_3-sheet0.png",
+		"images/photo_20231119_2-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
